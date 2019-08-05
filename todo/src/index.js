f2528f01be8c886b91f06f959d34ed88a6288b7f
akjeres <akjeres@gmail.com>
2019-08-05 20:28:53 +0300

    Lesson #25

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const el = (<div>
+    <h1>My Todo List</h1>
+    <input type="serach" placeholder="search" autoComplete="off" />
+    <ul>
+        <li>Learn JavaScript</li>
+        <li>Learn React</li>
+        <li>Build an App</li>
+        <li>Profit!</li>
+    </ul>
+</div>);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(el, rootElement);
