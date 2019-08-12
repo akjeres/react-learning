eebfdb4f9725e111b33fbecf24f2f13633c1bfc5
akjeres <akjeres@gmail.com>
2019-08-12 20:03:53 +0300

    Lesson 40. Immutable State. Removing item

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import AppHeader from '../app-header/';
 import SearchPanel from '../search-panel/';
 import TodoList from '../todo-list/';
 import ItemStatusFilter from '../item-status-filter/';
 
-const App = () => {
+export default class App extends Component {
+    constructor() {
+        super();
 
-    const todoData = [
-        { label: "Learn JavaScript", important: false, id: 0, },
-        { label: "Learn ReactJS", important: true, id: 1, },
-        { label: "Build ReactJS App", important: true, id: 2, },
-        { label: "Profit!", important: false, id: 3, },
-    ];
+        this.state = {
+            todoData: [
+                { label: "Learn JavaScript", important: false, id: 0, },
+                { label: "Learn ReactJS", important: true, id: 1, },
+                { label: "Build ReactJS App", important: true, id: 2, },
+                { label: "Profit!", important: false, id: 3, },
+            ],
+        };
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={ 1 } done={ 3 }/>
-            <div className='top-panel d-flex justify-content-between'>
-                <SearchPanel />
-                <ItemStatusFilter />
+        this.deleteItem = (id) => {
+            this.setState(( { todoData } ) => {                
+                return {
+                    todoData: todoData.filter((i) => i['id'] !== id),
+                };
+            });
+        };
+    }
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={ 1 } done={ 3 }/>
+                <div className='top-panel d-flex justify-content-between'>
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList 
+                    todos={ this.state.todoData } 
+                    onDeleted={ this.deleteItem }/>
             </div>
-            <TodoList 
-                todos={ todoData } 
-                onDeleted={ (id) => {
-                    console.log('del ', id);
-                } }/>
-        </div>
-    );
+        );
+    };
 };
-
-export default App;
