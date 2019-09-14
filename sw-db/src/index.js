0bde504164b4c070ee00085858da55fcafc593f6
akjeres <akjeres@gmail.com>
2019-09-14 10:07:05 +0300

    function-wrapper to fetch created

--- sw-db/src/index.js
+++ sw-db/src/index.js
@@ -1,1 +1,12 @@
-console.log(1);
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+
+    return body;
+}
+
+getResource('https://swapi.co/api/people/1/')
+    .then((body) => {
+        console.log(body);
+    });
