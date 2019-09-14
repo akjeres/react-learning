dd1a0cac4ac84bd725950bc662a18af2e9856ba4
akjeres <akjeres@gmail.com>
2019-09-14 16:06:24 +0300

    Random planet component added

--- sw-db/src/components/random-planet/random-planet.js
+++ sw-db/src/components/random-planet/random-planet.js
@@ -1,31 +1,67 @@
 import React, { Component } from 'react';
+import SWAPIService from '../../services/swapi-service';
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
+    state = {
+        id: null,
+        name: null,
+        population: null,
+        rotation_period: null,
+        diameter: null,
+    };
+
     constructor() {
         super();
-    }
+
+        this.swapiService = new SWAPIService();
+
+        this.updatePlanet();
+    };
+
+    updatePlanet = () => {
+        const id = Math.floor(Math.random() * 25) + 2;
+        this.swapiService.getPlanet(id)
+        .then((planet) => {
+            const { name, population, rotation_period, diameter } = planet;
+            this.setState({
+                id: id,
+                name: name,
+                population: population,
+                rotation_period: rotation_period,
+                diameter: diameter,
+            });
+        });
+    };
 
     render() {
+        const {
+            id,
+            name,
+            population,
+            rotation_period,
+            diameter,
+        } = this.state;
+
         return (
             <div className="random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src="https://starwars-visualguide.com/assets/img/planets/9.jpg"
+                     src={`https://starwars-visualguide.com/assets/img/planets/${ id }.jpg`}
                      alt="Coruscant"/>
                 <div>
-                    <h4>Coruscant</h4>
+                    <h4>{ name }</h4>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">
                             <span className="term">Population</span>
-                            <span>1,000,000,000,000</span>
+                            <span>{ population }</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Rotation Period</span>
-                            <span>24 days</span>
+                            <span>{ rotation_period } days</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Diameter</span>
-                            <span>12,240km</span>
+                            <span>{ diameter } km</span>
                         </li>
                     </ul>
                 </div>
